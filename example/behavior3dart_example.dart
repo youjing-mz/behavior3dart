7d382f22d8725f58271210bcf4a1478c212ad512
youjing <youjing@huya.com>
2022-11-04 14:39:31 +0800

    first commit

---
+++ example/behavior3dart_example.dart
@@ -0,0 +1,6 @@
+import 'package:behavior3dart/behavior3dart.dart';
+
+void main() {
+  var awesome = Awesome();
+  print('awesome: ${awesome.isAwesome}');
+}
---
+++ lib/behavior3dart.dart
@@ -0,0 +1,8 @@
+/// Support for doing something awesome.
+///
+/// More dartdocs go here.
+library behavior3dart;
+
+export 'src/behavior3dart_base.dart';
+
+// TODO: Export any libraries intended for clients of this package.
